8bd3d7ff325445d932e8673c508f4726a02c28d2
Josh Atkin <joshua.atkin1@gmail.com>
2023-01-25 20:46:58 +0000

    Create BaseProvider.php

---
+++ src/Providers/BaseProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Pixled\PasswordGenerator\Providers;
+
+use Closure;
+use Illuminate\Support\ServiceProvider;
+
+class BaseProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
